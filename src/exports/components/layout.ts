e69d38fe6a03361b93cdd96401b24d4377b08051
jsgithaka <jsgithaka@gmail.com>
2025-02-08 10:41:05 +0300

    feat: create and export layout components/styles
    Created the following components/style modules:
    1. Header
    2. Main
    3. Container
    4. Footer

---
+++ src/exports/components/layout.ts
@@ -0,0 +1,4 @@
+export { default as Header } from '@/components/layout/Header';
+export { default as Main } from '@/components/layout/Main';
+export { default as Footer } from '@/components/layout/Footer';
+export { default as Container } from '@/components/layout/Container';
